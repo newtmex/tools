24bcd0b966b8447d1b4572d4e6de578e4288c9d7
newtmex <brightomex@gmail.com>
2019-01-12 13:04:07 +0100

    initial commit

---
+++ btc_current_supply/README.md
@@ -0,0 +1,5 @@
+Run with `./btc_current_supply.js`
+If it does not work>>
+Make `btc_current_supply.js` an executable
+`chmod u+x ./btc_current_supply.js`
+Then run with `./btc_current_supply.js`
---
+++ btc_current_supply/btc_current_supply.js
@@ -0,0 +1,42 @@
+#!/usr/local/bin/node
+const halving_factor = 2;
+const initial_reward = 50;
+const block_halving = 210000; // Number of blocks before reward halving
+
+var block_height = 558216; // Current height; TODO add a way to get the current height automatically
+
+let blocks_to_next_halving = block_height % block_halving; // This could be equals to block_height if block_height <= block_halving
+let halving_times = Math.floor(block_height / block_halving);
+let current_reward = initial_reward / Math.pow(halving_factor, halving_times);
+let current_period = halving_times + 1;
+console.log({ current_reward });
+
+let current_supply, total_supply;
+total_supply = current_supply = current_reward * blocks_to_next_halving;
+
+let periodStats = [{
+  period: current_period,
+  period_reward: current_reward,
+  period_supply: current_supply,
+  halving_times: halving_times
+}]; // Populate with the current period
+
+// Add rewards from previous periods if this is not the first period
+for (let period = current_period; period > 1;) {
+  --period; // Did the reduction here so that it will reflect on the calculation
+  let halving_times = period - 1; // This will have different scope from the halving_times above
+  let period_reward = initial_reward / Math.pow(halving_factor, halving_times);
+  let period_supply = period_reward * block_halving; // Using block halving because it is expected that all the blocks for this period is already mined
+
+  periodStats.unshift({
+    period: period,
+    period_reward: period_reward,
+    period_supply: period_supply,
+    halving_times: halving_times
+  });
+
+  total_supply += period_supply;
+}
+
+console.log({ total_supply });
+console.log('Other stats:\n',periodStats);
