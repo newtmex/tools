e6c861eb1aea284b8ce35b36179ff5e600b81b41
newtmex <brightomex@gmail.com>
2019-01-12 21:39:38 +0100

    feature/Option for display sequence for 'Other stats'

--- btc_current_supply/btc_current_supply.js
+++ btc_current_supply/btc_current_supply.js
@@ -5,32 +5,31 @@ const block_halving = 210000; // Number of blocks before reward halving
 
 var block_height = 558216; // Current height; TODO add a way to get the current height automatically
 
-if(process.argv[2]){
+if (process.argv[2]) {
   ic(process.argv[2]);
-}else{
+} else {
   console.log(`
   Using default height: ${block_height}
   To run with custom height type:
-     ${process.argv[1]} [block_height]
-
+     ${process.argv[1]} [block_height] [-1]
+      the -1 is optional and is used to change the order of 'Other stats' section.
   `)
 }
-function ic(input){
+function ic(input) {
   let nInput = input;
   input = Number(input);
-    if(isNaN(input)){
-      console.log(`${nInput} is invalid. Please enter a number as [block_height].`);
-      process.exit();
-    } else {
-      block_height = input;
-    }
+  if (isNaN(input)) {
+    console.log(`${nInput} is invalid. Please enter a number as [block_height].`);
+    process.exit();
+  } else {
+    block_height = input;
+  }
 }
 
 let blocks_to_next_halving = block_height % block_halving; // This could be equals to block_height if block_height <= block_halving
 let halving_times = Math.floor(block_height / block_halving);
 let current_reward = initial_reward / Math.pow(halving_factor, halving_times);
 let current_period = halving_times + 1;
-console.log({ current_reward });
 
 let current_supply, total_supply;
 total_supply = current_supply = current_reward * blocks_to_next_halving;
@@ -41,6 +40,7 @@ let periodStats = [{
   period_supply: current_supply,
   halving_times: halving_times
 }]; // Populate with the current period
+console.log({ period_reward: periodStats[0].period_reward });
 
 // Add rewards from previous periods if this is not the first period
 for (let period = current_period; period > 1;) {
@@ -49,15 +49,24 @@ for (let period = current_period; period > 1;) {
   let period_reward = initial_reward / Math.pow(halving_factor, halving_times);
   let period_supply = period_reward * block_halving; // Using block halving because it is expected that all the blocks for this period is already mined
 
-  periodStats.unshift({
-    period: period,
-    period_reward: period_reward,
-    period_supply: period_supply,
-    halving_times: halving_times
-  });
+  if (process.argv[3] && process.argv[3] == -1) {
+    periodStats.push({
+      period: period,
+      period_reward: period_reward,
+      period_supply: period_supply,
+      halving_times: halving_times
+    });
+  } else {
+    periodStats.unshift({
+      period: period,
+      period_reward: period_reward,
+      period_supply: period_supply,
+      halving_times: halving_times
+    });
+  }
 
   total_supply += period_supply;
 }
 
 console.log({ total_supply });
-console.log('Other stats:\n',periodStats);
+console.log('Other stats:\n', periodStats);
